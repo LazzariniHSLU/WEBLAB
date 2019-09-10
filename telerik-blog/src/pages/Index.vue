23de5961181dc72d5e527a627c4144d1d3a8b7a3
Flavio Lazzarini <flavio@lazzarini.li>
2019-09-10 15:09:32 +0200

    init simple blog example

---
+++ telerik-blog/src/pages/Index.vue
@@ -0,0 +1,52 @@
+<template>
+  <Layout>
+    <header class="header">
+      <h1>Gridsome Blog</h1>
+      <p>A simple blog designed with Gridsome</p>
+    </header>
+    <section class="posts">
+      <PostList v-for="edge in $page.allPost.edges" :key="edge.node.id" :post="edge.node" />
+    </section>
+  </Layout>
+</template>
+
+<script>
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList
+  },
+  metaInfo: {
+    title: "A simple blog"
+  }
+};
+</script>
+
+<page-query>
+query {
+  metaData {
+    siteName
+    siteDescription
+  }
+  allPost {
+    totalCount
+    edges {
+      node {
+        id
+        title
+        timeToRead
+        description
+        date (format: "D MMMM YYYY")
+        path
+      }
+    }
+
+  }
+}
+</page-query>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
